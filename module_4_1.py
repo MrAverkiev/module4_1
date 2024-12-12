8e31262e864aed9dd30786a0e00e0bb2da5c97e8
MrAverkiev <vaverkiev1@gmail.com>
2024-12-12 14:49:42 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,15 @@
+import fake_math as fm
+import true_math as tm
+
+fake_divide = fm.fake_divide
+true_divide = tm.true_divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,5 @@
+from math import inf
+def true_divide(first, second):
+    if second==0:
+        return (inf)
+    else:return first/second
